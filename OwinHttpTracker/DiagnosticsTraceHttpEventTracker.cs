b1c902f87809162c9276edc5c3bb18789281f5cb
J Reed <jreed@trialcard.com>
2017-09-24 14:25:01 -0400

    Adding a tracker that emits straight to Diagnostics Tracing for developer convenience (can see in Visual Studio).

---
+++ OwinHttpTracker/DiagnosticsTraceHttpEventTracker.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Cache;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OwinHttpTracker
+{
+    public class DiagnosticsTraceHttpEventTracker : IHttpEventTracker
+    {
+        public void EmitEvent(HttpEvent httpEvent)
+        {
+            var eventAsJson = Newtonsoft.Json.JsonConvert.SerializeObject(httpEvent);
+            System.Diagnostics.Trace.WriteLine(eventAsJson);
+
+            System.Diagnostics.Trace.WriteLine(httpEvent.Request);
+            System.Diagnostics.Trace.WriteLine(httpEvent.Response);
+        }
+    }
+}
